edf1a18019f6434497bfc0c57d307f4c000ed922
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2023-02-10 09:03:02 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+/*
+ * www.codeurjava.com
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+        final Socket clientSocket;
+        final BufferedReader in;
+        final PrintWriter out;
+        final Scanner sc = new Scanner(System.in);//pour lire à partir du clavier
+
+        try {
+            /*
+             * les informations du serveur ( port et adresse IP ou nom d'hote
+             * 127.0.0.1 est l'adresse local de la machine
+             */
+            clientSocket = new Socket("10.200.0.184",5000);
+
+            //flux pour envoyer
+            out = new PrintWriter(clientSocket.getOutputStream());
+            //flux pour recevoir
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            Thread envoyer = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    while(true){
+                        msg = sc.nextLine();
+                        out.println(msg);
+                        out.flush();
+                    }
+                }
+            });
+            envoyer.start();
+
+            Thread recevoir = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    try {
+                        msg = in.readLine();
+                        while(msg!=null){
+                            System.out.println("Serveur : "+msg);
+                            msg = in.readLine();
+                        }
+                        System.out.println("Serveur déconecté");
+                        out.close();
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            recevoir.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
